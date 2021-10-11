538d4fe06994479f3587fe8eeb7d9c46b6b2884b
Petr Rybnikov <ribnikov.peter@yandex.ru>
2021-10-11 07:44:54 +0700

    Add file 1.py Add file 2.py

--- lab3/1.py
+++ lab3/1.py
@@ -1,19 +1,37 @@
 import pygame
 from pygame.draw import *
 
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((400, 400))
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+sc = pygame.display.set_mode((640, 480))
+sc.fill((255, 255, 255))
+
+circle(sc, YELLOW, (320, 240), 100)
+circle(sc, BLACK, (320, 240), 100, 1)
+
+circle(sc, RED, (270, 210), 20)
+circle(sc, BLACK, (270, 210), 20, 1)
+circle(sc, BLACK, (270, 210), 10)
+
+circle(sc, RED, (370, 210), 16)
+circle(sc, BLACK, (370, 210), 16, 1)
+circle(sc, BLACK, (370, 210), 6)
+
+rect(sc, BLACK, (280, 280, 80, 15))
+
+line(sc, BLACK, [220, 170], [300, 190], 10)
+
+line(sc, BLACK, [420, 170], [340, 190], 10)
+
+# polygon(sc, BLACK, [[300, 250], [320, 280],
+#                      [190, 190], [130, 130]])
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
